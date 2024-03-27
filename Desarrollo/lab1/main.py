388fa36e12602ad5d1c518cadd8b984bdd1cdae9
bluemili <camifig17@gmail.com>
2024-03-27 17:58:37 -0300

    Se elimina carpetas duplicadas

--- Desarrollo/lab1/main.py
+++
@@ -1,6 +0,0 @@
-from animales import animales
-
-if __name__ == '__main__':
-    print("Ingrese el animalito para saber que dice:")
-    animal = input()
-    animales(animal)
--- Desarrollo/lab1/readme.md
+++
@@ -1,3 +0,0 @@
-Proyecto para emular los sonidos de animales
-
-![Screenshot](https://www.todopapas.com/img/cuentos/1/3/2/2/1322.jpg)
